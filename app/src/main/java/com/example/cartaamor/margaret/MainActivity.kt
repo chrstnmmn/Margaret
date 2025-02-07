9069899c9310a5a45eec4532d484ca16c3987710
Christian Emmanuel <christianemmanuelcollo23@gmail.com>
2025-02-07 10:02:03 +0800

    learning how to use android studio

--- app/src/main/java/com/example/cartaamor/margaret/MainActivity.kt
+++ app/src/main/java/com/example/cartaamor/margaret/MainActivity.kt
@@ -4,44 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cartaamor.margaret.ui.theme.MargaretTheme
+import com.example.cartaamor.margaret.ui.theme.Pink80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MargaretTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MargaretTheme{}
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    // using the surface container to add a background color
+    Surface(color = Pink80) {
+        Text(
+            text = "I love you $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MargaretTheme {
-        Greeting("Emman")
+        Greeting("My Margaret")
     }
 }
